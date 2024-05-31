637f5b714bc39106e5c0088857798b820668708d
vbjhbj <119402788+vbjhbj@users.noreply.github.com>
2024-05-31 12:21:34 +0200

    end of lesson

--- modules/mod_education/mod_education.php
+++ modules/mod_education/mod_education.php
@@ -15,6 +15,31 @@ $secDip = $params->get('secondaryd');
 $secSchool = $params->get('secondary');
 $secStart = $params->get('secondarys');
 $secEnd = $params->get('secondaryf');
+$uni1Dip = $params->get('uni1d');
+$uni1School = $params->get('uni1');
+$uni1Start = $params->get('uni1s');
+$uni1End = $params->get('uni1f');
+$uni2Dip = $params->get('uni2d');
+$uni2School = $params->get('uni2');
+$uni2Start = $params->get('uni2s');
+$uni2End = $params->get('uni2f');
+$uni3Dip = $params->get('uni3d');
+$uni3School = $params->get('uni3');
+$uni3Start = $params->get('uni3s');
+$uni3End = $params->get('uni3f');
+$uni4Dip = $params->get('uni4d');
+$uni4School = $params->get('uni4');
+$uni4Start = $params->get('uni4s');
+$uni4End = $params->get('uni4f');
+$uni5Dip = $params->get('uni5d');
+$uni5School = $params->get('uni5');
+$uni5Start = $params->get('uni5s');
+$uni5End = $params->get('uni5f');
+$uni6Dip = $params->get('uni6d');
+$uni6School = $params->get('uni6');
+$uni6Start = $params->get('uni6s');
+$uni6End = $params->get('uni6f');
+
 $showYears = $params->get('schoolstartyears');
 
 
--- modules/mod_education/tmpl/default.php
+++ modules/mod_education/tmpl/default.php
@@ -12,11 +12,31 @@ defined('_JEXEC') or die;
 <div class="education-module-wrap<?php echo $moduleclass_sfx; ?>">
     <div class="education-module-wrap-inner">
         
-    <?php if ($secDip != null) :?>
+    <?php if ($secDip != "") :?>
         <h1>Secondary Education</h1>
         <p><?php echo $secSchool ?></p>
         <p>
             <?php echo $secDip ?>
+            <?php if ($showYears) :?>
+                <?php echo $secStart ?>
+                -
+            <?php endif?>
+            <?php echo $secEnd ?>
+        </p>
+    <?php endif;?>
+
+    <?php if ($uni1Dip != "") :?>
+        <?php if ($secDip != "") :?>
+            <h1>University Education</h1>
+        <?php endif?>
+        <p><?php echo $uni1School ?></p>
+        <p>
+            <?php echo $uni1Dip ?>
+            <?php if ($showYears) :?>
+                <?php echo $uni1Start ?>
+                -
+            <?php endif?>
+            <?php echo $uni1End ?>
         </p>
     <?php endif;?>
         
